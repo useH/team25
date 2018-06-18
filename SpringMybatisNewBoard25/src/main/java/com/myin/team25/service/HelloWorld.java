54251153642bd46c5fef498ba6ed71611bd08d59
USER <USER@com201-05>
2018-06-18 16:02:16 +0900

    HelloWorld_Git_test

---
+++ SpringMybatisNewBoard25/src/main/java/com/myin/team25/service/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.myin.team25.service;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("제발 그냥 되라!!!");
+
+	}
+
+}
